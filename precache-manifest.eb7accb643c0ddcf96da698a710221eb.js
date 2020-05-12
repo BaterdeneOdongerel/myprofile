fb7c5393f68d91249a310e6da80bae3c5b2f88b1
Bati <bati.odon@gmail.com>
2020-05-12 15:48:13 -0500

    Updates

---
+++ precache-manifest.eb7accb643c0ddcf96da698a710221eb.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "551a7b0499892e09fb65da844bf5b730",
+    "url": "/myprofile/index.html"
+  },
+  {
+    "revision": "7d549f4cdf78c788095d",
+    "url": "/myprofile/static/css/main.347439cb.chunk.css"
+  },
+  {
+    "revision": "c509ec726f99033e630d",
+    "url": "/myprofile/static/js/2.c11c0722.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/myprofile/static/js/2.c11c0722.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d549f4cdf78c788095d",
+    "url": "/myprofile/static/js/main.66f490b9.chunk.js"
+  },
+  {
+    "revision": "e92310c2eb859906b202",
+    "url": "/myprofile/static/js/runtime-main.958da196.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/myprofile/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "6cb9319240c19dc9ee2dcff2ddade248",
+    "url": "/myprofile/static/media/me.6cb93192.jpg"
+  }
+]);
