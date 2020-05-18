78f15e3d0d16b51b6a4cda39464868b4d79ff96e
Bati <bati.odon@gmail.com>
2020-05-17 21:58:11 -0500

    d

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import React from 'react';
 import BaseLayout from './components/BaseLayout';
 
 function App() {
+  React.useEffect(() => {
+      document.title = "Baterdene Odongerel"
+  })
   return <BaseLayout />;
 }
 
